b0bc92052f06a8dd640dbdf5c045258aa1a04b2b
VInicius de Freitas Pereira <vfreitas@Viniciuss-MacBook-Pro.local>
2025-07-03 17:13:19 -0300

    Update task queue and restructure worker setup

--- writing_workflow/main.py
+++ writing_workflow/main.py
@@ -1,12 +1,18 @@
-import sys
 import asyncio
+
+from temporalio.client import Client
+from temporalio.worker import Worker
+
 from workflow import GreetSomeone
 
 async def main():
-    name = sys.argv[1]
-    greeter = GreetSomeone()
-    greeting = await greeter.run(name)
-    print(greeting)
+    # make a client and connect to running instance (server, not the UI or metrics)
+    client = await Client.connect("localhost:7233", namespace="default")
+
+    # Run the worker
+    worker = Worker(client, task_queue="greeting-tasks", workflows=[GreetSomeone])
+    await worker.run()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
